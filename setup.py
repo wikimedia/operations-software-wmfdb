094fd547a6ae7c0597159e270c43443dd91591ba
Stevie Beth Mhaol <sbmhaol@wikimedia.org>
2021-11-25 15:11:23 +0100

    Initial structure and configs.
    Change-Id: I965a06f44de63b23295bde3003f216eb7736c854

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""wmfdb."""
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wmfdb",
+    description="wmfdb",
+    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gerrit.wikimedia.org/r/admin/repos/operations%2Fsoftware%2Fwmfdb",
+    packages=("wmfdb",),
+    install_requires=["pymysql>=0.9.3"],
+    test_suite="wmfdb.tests",
+)
