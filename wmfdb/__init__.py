55501131882ad4661a6b53ff0f4d1e3657fb9556
Kormat <sbmhaol@wikimedia.org>
2024-05-10 09:03:14 +0000

    Move away from deprecated pkg_resources
    pkg_resources is deprecated, and replaced by importlib.metadata:
    
        https://setuptools.pypa.io/en/latest/pkg_resources.html
    
    See merge request repos/sre/wmfdb!11

--- wmfdb/__init__.py
+++ wmfdb/__init__.py
@@ -1,8 +1,8 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib import metadata
 
 try:
     # Must be the same used as 'name' in setup.py
-    __version__ = get_distribution("wmfdb").version
+    __version__ = metadata.version("wmfdb")
     """:py:class:`str`: the version of the current wmfdb module."""
-except DistributionNotFound:  # pragma: no cover - this should never happen during tests
+except metadata.PackageNotFoundError:  # pragma: no cover - this should never happen during tests
     pass  # package is not installed
